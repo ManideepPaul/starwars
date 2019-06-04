ef891379f6a86915e7d801aec69c6eee9d09af33
ManideepPaul <you@example.com>
2019-06-04 22:28:20 +0530

    Updates

---
+++ precache-manifest.2564ffffb4e45f96cd517f75ca93654a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f862c7fa4c4b51bd3c5b1c198eda3f6",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/starwars/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fd18fd9afdfa4a170191",
+    "url": "/starwars/static/css/main.0820dbd1.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/starwars/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "fd18fd9afdfa4a170191",
+    "url": "/starwars/static/js/main.7848c2df.chunk.js"
+  },
+  {
+    "revision": "9cd712b42b70d05ab420",
+    "url": "/starwars/static/js/runtime~main.f4b6364c.js"
+  },
+  {
+    "revision": "85a83ad5f03fccd2b4dee24e797fdbf0",
+    "url": "/starwars/static/media/space.85a83ad5.jpg"
+  }
+]);
